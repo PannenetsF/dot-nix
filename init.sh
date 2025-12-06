9204bcd9ebb810efada18062448ac56b3c6a8bb1
Pannenets F <pannenets.f@foxmail.com>
2025-12-06 19:22:28 +0800

    add proxy

--- init.sh
+++ init.sh
@@ -8,10 +8,19 @@ git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o Stri
 
 mkdir -m 0755 -p /nix && groupadd -r nixbld && chown root /nix \
       && for n in $(seq 1 10); do useradd -c "Nix build user $n" -d /var/empty -g nixbld -G nixbld -M -N -r -s "$(command -v nologin)" "nixbld$n"; done
+
+[[ -n "$PF_http_proxy" ]] && export http_proxy=$PF_http_proxy
+[[ -n "$PF_https_proxy" ]] && export https_proxy=$PF_https_proxy
+[[ -n "$PF_no_proxy" ]] && export no_proxy=$PF_no_proxy
+
 sh <(curl --proto '=https' --tlsv1.2 -L https://mirrors.tuna.tsinghua.edu.cn/nix/latest/install) --no-daemon --no-channel-add
 USER=root . $HOME/.nix-profile/etc/profile.d/nix.sh
 echo "USER=root . \$HOME/.nix-profile/etc/profile.d/nix.sh" >> ~/.bashrc
 source ~/.bashrc
+
+[[ -n "$PF_http_proxy" ]] && export http_proxy=$PF_http_proxy
+[[ -n "$PF_https_proxy" ]] && export https_proxy=$PF_https_proxy
+[[ -n "$PF_no_proxy" ]] && export no_proxy=$PF_no_proxy
 
 nix-channel --add https://mirrors.tuna.tsinghua.edu.cn/nix-channels/nixpkgs-unstable nixpkgs
 nix-channel --update
