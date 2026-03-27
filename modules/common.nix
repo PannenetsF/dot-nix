c74eb4ac4e54107dcb12a65b5aba0c2f5a6844dc
PF <pannenets.f@foxmail.com>
2026-03-27 13:56:49 +0800

    refactor: extract common home-manager module

---
+++ modules/common.nix
@@ -0,0 +1,71 @@
+{ config, pkgs, pkgsUnstable, lib, ... }:
+{
+  home = {
+    packages = [
+      pkgs.lazygit
+      pkgs.git
+      pkgs.tmux
+      pkgs.wget
+      pkgs.curl
+      pkgs.lua
+      pkgs.gdb
+      pkgs.nixfmt-classic
+      pkgsUnstable.clang-tools
+      pkgsUnstable.fzf
+      pkgsUnstable.ripgrep
+      pkgs.nodePackages.vim-language-server
+      pkgs.universal-ctags
+    ];
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "robbyrussell";
+    };
+
+    initExtra = ''
+      # <<<<< Enable natural text editing
+      #
+      # Move to the beginning of the line. `Cmd + Left Arrow`:
+      bindkey "^[[1;9D" beginning-of-line
+      # Move to the end of the line. `Cmd + Right Arrow`:
+      bindkey "^[[1;9C" end-of-line
+      # Move to the beginning of the previous word. `Option + Left Arrow`:
+      bindkey "^[[1;3D" backward-word
+      # Move to the beginning of the next word. `Option + Right Arrow`:
+      bindkey "^[[1;3C" forward-word
+      # Delete the word behind the cursor. `Option + Delete`:
+      bindkey "^[[3;10~" backward-kill-word
+      # Delete the word after the cursor. `Option + fn + Delete`:
+      bindkey "^[[3;3~" kill-word
+      #
+      # Enable natural text editing >>>>>
+    '';
+  };
+
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+    package = pkgsUnstable.neovim-unwrapped;
+  };
+
+  programs.tmux = {
+    extraConfig = ''
+      set-option -a terminal-features "xterm:RGB"
+    '';
+  };
+
+  home.sessionVariables = {
+    PATH = "$HOME/.local/bin:$PATH";
+    LC_ALL = "C.UTF-8";
+  };
+
+  programs.home-manager.enable = true;
+}
