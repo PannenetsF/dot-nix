0ac56bdcb9ae4852989f585e53c36b3aa1725ec6
fanyunqian.1 <fanyunqian.1@bytedance.com>
2026-06-13 00:07:54 +0800

    refactor: pass home identity through flake args

--- flake.nix
+++ flake.nix
@@ -31,8 +31,30 @@
           inherit system;
           config = nixpkgsConfig;
         };
+      mkUserHome = system:
+        let
+          nixHmUser = builtins.getEnv "NIX_HM_USER";
+          nixHmHome = builtins.getEnv "NIX_HM_HOME";
+          envUser =
+            if nixHmUser != "" then nixHmUser else builtins.getEnv "USER";
+          envHome =
+            if nixHmHome != "" then nixHmHome else builtins.getEnv "HOME";
+          isDarwin = builtins.match ".*-darwin" system != null;
+          inferredHome = if envUser == "" then
+            ""
+          else if isDarwin then
+            "/Users/${envUser}"
+          else if envUser == "root" then
+            "/root"
+          else
+            "/home/${envUser}";
+        in {
+          username = envUser;
+          homeDir = if envHome != "" then envHome else inferredHome;
+        };
       mkHomeConfig = { system, isHost ? false, }:
         let
+          userHome = mkUserHome system;
           pkgs = mkPkgs system;
           pkgsUnstable = mkPkgsUnstable system;
         in home-manager.lib.homeManagerConfiguration {
@@ -41,22 +63,18 @@
             inherit pkgsUnstable;
             inherit system;
             inherit isHost;
+            inherit (userHome) username homeDir;
           };
 
           modules = [ ./home.nix ];
         };
       mkDarwinConfig = { system, }:
-        let
-          nixHmUser = builtins.getEnv "NIX_HM_USER";
-          nixHmHome = builtins.getEnv "NIX_HM_HOME";
-          username =
-            if nixHmUser != "" then nixHmUser else builtins.getEnv "USER";
-          homeDir =
-            if nixHmHome != "" then nixHmHome else builtins.getEnv "HOME";
+        let userHome = mkUserHome system;
         in nix-darwin.lib.darwinSystem {
           inherit system;
           specialArgs = {
-            inherit system username homeDir;
+            inherit system;
+            inherit (userHome) username homeDir;
             pkgsUnstable = mkPkgsUnstable system;
           };
           modules = [
--- home.nix
+++ home.nix
@@ -1,38 +1,28 @@
-{ config, pkgs, pkgsUnstable, lib, system, isHost ? false, ... }: {
+{ config, pkgs, pkgsUnstable, lib, system, username, homeDir, isHost ? false
+, ... }: {
   # NOTE: Home Manager expects `home.homeDirectory` to be an absolute path.
-  # We prefer $HOME (works for both macOS/Linux), and fall back to a reasonable
-  # default based on `system` + $USER when $HOME is unavailable (e.g. pure eval).
-  # If neither is available, fail with a clear error.
+  # flake.nix derives these values once from NIX_HM_* / USER / HOME and passes
+  # them here explicitly. Keep this module free of direct environment reads.
   _module.args = let
     debug = builtins.getEnv "NIX_HM_DEBUG" == "1";
-    userEnv = builtins.getEnv "NIX_HM_USER";
-    homeOverride = builtins.getEnv "NIX_HM_HOME";
-    user = if userEnv != "" then userEnv else builtins.getEnv "USER";
-    homeEnv =
-      if homeOverride != "" then homeOverride else builtins.getEnv "HOME";
-    isDarwin = builtins.match ".*-darwin" system != null;
-    inferredHome = if user == "" then
-      ""
-    else if isDarwin then
-      "/Users/${user}"
-    else if user == "root" then
-      "/root"
-    else
-      "/home/${user}";
-    homeDirRaw = if homeEnv != "" then homeEnv else inferredHome;
-    homeDir = if debug then
+    tracedHomeDir = if debug then
       builtins.trace
-      "[home.nix][debug] system=${system} USER='${user}' HOME='${homeEnv}' NIX_HM_HOME='${homeOverride}' inferredHome='${inferredHome}' homeDir='${homeDirRaw}'"
-      homeDirRaw
-    else
-      homeDirRaw;
-  in {
-    _user = user;
-    _homeDir = if homeDir != "" && builtins.substring 0 1 homeDir == "/" then
+      "[home.nix][debug] system=${system} username='${username}' homeDir='${homeDir}'"
       homeDir
     else
+      homeDir;
+  in {
+    _user = if username != "" then
+      username
+    else
       throw
-      "home.homeDirectory is empty or not absolute; please set HOME/USER env or hardcode home.homeDirectory";
+      "home.username is empty; please set USER or NIX_HM_USER before evaluating";
+    _homeDir =
+      if tracedHomeDir != "" && builtins.substring 0 1 tracedHomeDir == "/" then
+        tracedHomeDir
+      else
+        throw
+        "home.homeDirectory is empty or not absolute; please set HOME or NIX_HM_HOME before evaluating";
   };
 
   imports = [ ./modules/common.nix ]
