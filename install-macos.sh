829aefe8a4ad3bf22b67e0bc8a2c27f343002eef
Pannenets F <pannenets.f@foxmail.com>
2026-06-13 12:29:57 +0800

    fix: sync kitty config during macos activation

--- install-macos.sh
+++ install-macos.sh
@@ -2,6 +2,25 @@
 set -e
 
 FILE_LOCK="$HOME/pf-init-macos"
+KITTY_CONFIG_REPO="${KITTY_CONFIG_REPO:-https://github.com/PannenetsF/dot-kitty.git}"
+KITTY_CONFIG_BRANCH="${KITTY_CONFIG_BRANCH:-main}"
+
+sync_git_config() {
+  local repo="$1"
+  local branch="$2"
+  local dest="$3"
+
+  mkdir -p "$(dirname "$dest")"
+  if [ -d "$dest/.git" ]; then
+    git -C "$dest" fetch origin "$branch"
+    git -C "$dest" checkout -B "$branch" "origin/$branch"
+    git -C "$dest" reset --hard "origin/$branch"
+    git -C "$dest" clean -fd
+  else
+    rm -rf "$dest"
+    git clone --branch "$branch" "$repo" "$dest"
+  fi
+}
 
 PIP_EXTRA=()
 if [ -n "$PIP_POSTFIX" ]; then
@@ -9,6 +28,8 @@ if [ -n "$PIP_POSTFIX" ]; then
   # shellcheck disable=SC2206
   PIP_EXTRA=($PIP_POSTFIX)
 fi
+
+sync_git_config "$KITTY_CONFIG_REPO" "$KITTY_CONFIG_BRANCH" "$HOME/.config/kitty"
 
 if [ -f "$FILE_LOCK" ]; then
   echo "All py packages are installed yet."
